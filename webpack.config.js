18e484df6317e5cf57da7ab34eceba887bb82b8e
analuciabolico <analuciabolico.dev@gmail.com>
2022-06-02 01:03:23 -0300

    fix: path wrong on generate build folder

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
   paths: (paths) => {
     paths.appBuild = path.resolve(
       __dirname,
-      "..",
+      ".",
       process.env.REACT_APP_OUTPUT_PATH || "build"
     );
 
